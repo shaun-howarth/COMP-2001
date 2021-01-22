79ffdb446f586dd297d9b6bb498c8179b96f49af
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-22 17:32:39 +0000

    Have made PHP Storm project for Web Data Link application
    Have created a folder named "Web App" in the project/repo directory. Have made two subfolders within the web app folder called "assets" & "pages". Have made a start on index.php page.

---
+++ Web App/pages/index.php
@@ -0,0 +1,25 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="..">
+
+
+    <meta charset="UTF-8">
+    <title>Plymouth Crimes</title>
+</head>
+
+<body>
+
+
+<div class="wrapper">
+    <div class="title-card">
+
+    </div>
+</div>
+
+</body>
+</html>
