17f239423ef3ac5bb891be5413dbb2163c10dc44
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-30 20:04:00 +0000

    Have entered text about the Product Vision.
    Entered text into the right "body-card" using span text. Information is about the Product Vision.

--- Web App/pages/index.php
+++ Web App/pages/index.php
@@ -15,26 +15,38 @@
 <body>
 
 <div class="title-card">
-    <span class="title-text">Crime- Plymouth 2003-2015 Dataset</span>
+    <span class="title-text">Crime - Plymouth 2003-2015 Dataset</span>
 
 </div>
-
-
-
-<span class="body-text">hello</span>
-
 
 
 <div class="wrapper">
 
-    <div class="body-card">
 
-        <span class="body-text">Dataset (CSV & Formatted)</span>
+    <div class="body-card">
+        <span class="subtitle-text">Dataset (CSV & Formatted)</span>
+
+        <span class="body-text">
+        </span>
     </div>
 
 
 
+    <div class="body-card">
+        <span class="subtitle-text">Product Vision</span>
+
+        <span class="body-text">This linked data web application serves the purpose of displaying a chosen dataset in two different formats.
+            One being a human readable format (HTML table), and RDF format (CSV). The selected dataset contains data showing the amount and type of crime committed in Plymouth from the years 2003-2015.
+        </span>
+    </div>
+
+
+
+
 </div>
+
+
+
 
 
 
