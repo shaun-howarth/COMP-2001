08e7a21a1841fe911d63e5faf67f380830800222
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-31 11:50:41 +0000

    Have created data.php page.
    On this page, this is where the human readable format for the CSV crime dataset will be shown within a HTML table.

---
+++ Web App/pages/data.php
@@ -0,0 +1,26 @@
+<?php
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+    <meta charset="UTF-8">
+    <title>View Dataset: Human Readable Format</title>
+
+</head>
+
+<body>
+
+
+<div class="wrapper">
+    <div class="title-card">
+
+    </div>
+</div>
+
+</body>
+</html>
