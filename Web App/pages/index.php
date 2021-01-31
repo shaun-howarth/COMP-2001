b8dca4b81eac44db457748a1a512091dc667627d
Shaun Howarth <57568918+shaun-howarth@users.noreply.github.com>
2021-01-31 10:41:06 +0000

    Have created CSS classes for text stored in body cards.
    Have created three CSS classes for text appearence and formatting. "subtitle-text" span class used for the subtitle text in the body cards. "subtitle-align" div class for wrapping "subtitle-text" into the center. Finally, "margin-space", to leave a gap between text or other content.

--- Web App/pages/index.php
+++ Web App/pages/index.php
@@ -20,40 +20,38 @@
 </div>
 
 
-
-
-
 <div class="wrapper">
 
 
     <div class="body-card">
         <span class="subtitle-text">Dataset (CSV & Formatted)</span>
-
-
     </div>
 
 
 
     <div class="body-card">
 
+        <div class="subtitle-align">
+            <span class="subtitle-text">Dataset (In Original Format)</span>
+        </div>
+
         <a href="https://plymouth.thedata.place/dataset/crime/resource/26904f63-e13a-450c-85c7-954b66229871">
             <button>Link to original Dataset</button>
         </a>
 
-        <span class="subtitle-text">Product Vision</span>
+        <div class="subtitle-align">
+            <span class="subtitle-text">Product Vision</span>
+        </div>
 
-        <span class="body-text">This linked data web application serves the purpose of displaying a chosen dataset in two different formats.
-            One being a human readable format (HTML table), and RDF format (CSV). The selected dataset contains data showing the amount and type of crime committed in Plymouth from the years 2003-2015.
-        </span>
+        <div class="margin-space">
+            <span class="body-text">This linked data web application serves the purpose of displaying a chosen dataset in two different formats.
+                One being a human readable format (HTML table), and RDF format (CSV). The selected dataset contains data showing the amount and type of crime committed in Plymouth from the years 2003-2015.
+            </span>
+        </div>
     </div>
 
 
-
-
 </div>
-
-
-
 
 
 
